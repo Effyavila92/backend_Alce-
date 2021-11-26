bda7ff56706a7777e77f3459be95c232b2e2708e
tefy_ <savila921@gmail.com>
2021-11-25 19:19:04 -0500

    subida de archivos de spring.io

---
+++ src/main/java/com/misiontic/alcesteMs/AlcesteMsApplication.java
@@ -0,0 +1,13 @@
+package com.misiontic.alcesteMs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcesteMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlcesteMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/misiontic/alcesteMs/AlcesteMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misiontic.alcesteMs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlcesteMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
